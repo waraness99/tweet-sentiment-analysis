be3d5aea842e324b18e396155fe8314eb4d96c3f
Constant Druon <druon.constant@gmail.com>
2022-10-16 02:10:57 +0200

    serverless function with external libraries

---
+++ server/README.md
@@ -0,0 +1,3 @@
+# Tweet Sentiment Analysis Server
+
+This is the server for the Tweet Sentiment Analysis project. It is a simple GET API that takes a tweet as a parameter and returns the sentiment of the tweet.
--- server/api/index.py
+++ server/api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import re
 
 class handler(BaseHTTPRequestHandler):
 
@@ -13,7 +14,31 @@ class handler(BaseHTTPRequestHandler):
 		if "name" in dic:
 			message = "Hello, " + dic["name"] + "!"
 		else:
-			message = "Hello, stranger!"
+			message = text_preprocessing("Hell ! 3 @eeee frèreHHH")
 
 		self.wfile.write(message.encode())
 		return
+
+def text_preprocessing(text):
+     # lowercase
+    text = text.lower()
+    # remove extra whitespaces
+    text = " ".join(text.split())
+    # remove urls
+    text = re.sub(r'http\S+', '', text)
+    # remove identifiers (e.g. @user)
+    text = re.sub(r'@\S+', '', text)
+    # replace common contractions by full words
+    # text = contractions.fix(text)
+    # # remove punctuation
+    # text = text.translate(str.maketrans('', '', string.punctuation))
+    # remove special characters
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    # remove numbers
+    text = re.sub(r'\d+', '', text)
+    # remove small words
+    text = re.sub(r'\b\w{1,2}\b', '', text)
+    # spell correction (takes too much time, and doesn't improve that much accuracy)
+    # text = Speller(lang='en', fast=True)(text)
+    
+    return text
